88395ad1cec1a3e8d25a23f1448975a26056300f
oliverlai55 <oliverlai55@gmail.com>
2015-11-25 11:39:45 -0500

    adding custom directory

--- controller.js
+++ controller.js
@@ -2,6 +2,25 @@ var stateMap = angular.module('interactiveMap', []);
 
 stateMap.controller('interactiveMapCntrl', interactiveMapCntrl);
 
+stateMap.directive('clickState', function(){
+	return{
+		link: function($scope, element, iAttrs){
+			// console.log(iAttrs);
+		
+			element.bind('click', function(){
+				console.log($scope.state)
+				var newColor = "red";
+				var pathOfTheElement = element[0].querySelector('path')
+				console.log(pathOfTheElement);
+				pathOfTheElement.setAttribute('class',newColor);
+
+			});
+		}
+	}
+});
+
+
+
 function interactiveMapCntrl($scope){
 	$scope.states = states;
 }
